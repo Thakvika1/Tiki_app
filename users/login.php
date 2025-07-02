46cfbf9918fbeb916a69aa1bd314aaa4722f4e52
Thakvika1 <thakvika.thorn@gmail.com>
2025-07-02 19:57:27 +0700

    connect to database in login file

---
+++ users/login.php
@@ -0,0 +1,13 @@
+
+<?php 
+    include "../conn_db.php";
+
+    $conn = mysqli_connect($host, $user, $password, $database);
+
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    echo "Connected successfully! <br /><br />";
+
+?>
