51107fbf987f3dd5ccb74716dc87132804c3681b
Thakvika1 <thakvika.thorn@gmail.com>
2025-07-07 01:17:28 +0700

    add button

--- products/product_card.php
+++ products/product_card.php
@@ -7,6 +7,10 @@
                 <img id='image' src='$image' />
                 <h3>$title</h3>
                 <p>$$price</p>
+                <form method='get'>
+                    <button>Add To Cart</button>
+                    <button>Buy Now</button>
+                </form>
             </div>
         ";
     }
