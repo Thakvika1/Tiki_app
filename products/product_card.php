94f3a45a51932d7eee7695ca7bc7203f6fafdaa1
Thakvika1 <thakvika.thorn@gmail.com>
2025-07-08 12:09:32 +0700

    refactor product card

--- products/product_card.php
+++ products/product_card.php
@@ -1,15 +1,16 @@
 
 
 <?php
-    function productCard($title, $price, $image) {
+    function productCard($title, $price, $image, $product_id) {
         echo "
             <div class='product'>
                 <img id='image' src='$image' />
                 <h3>$title</h3>
                 <p>$$price</p>
-                <form method='get'>
-                    <button>Add To Cart</button>
-                    <button>Buy Now</button>
+                <form method='get' action=''>
+                    <input value='$product_id' type='hidden' />
+                    <button name='add_to_card' type='submit'>Add To Cart</button>
+                    <button name='buy_now' type='submit'>Buy Now</button>
                 </form>
             </div>
         ";
