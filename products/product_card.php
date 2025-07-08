21686b9ed3fb534120ce66759fa4c04c86f708f4
Thakvika1 <thakvika.thorn@gmail.com>
2025-07-08 12:16:26 +0700

    create confirm order and refactor product card

--- products/product_card.php
+++ products/product_card.php
@@ -7,7 +7,7 @@
                 <img id='image' src='$image' />
                 <h3>$title</h3>
                 <p>$$price</p>
-                <form method='get' action=''>
+                <form method='get' action='../orders/confirm_order.php'>
                     <input value='$product_id' type='hidden' />
                     <button name='add_to_card' type='submit'>Add To Cart</button>
                     <button name='buy_now' type='submit'>Buy Now</button>
