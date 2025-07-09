f54e9365cca17abae437053f1e48c6293307c819
Thakvika1 <thakvika.thorn@gmail.com>
2025-07-10 00:41:39 +0700

    create table_component

--- admin/table_component.php
+++ admin/table_component.php
@@ -1,10 +1,42 @@
 
 <?php
 
-    // connect to database
-    include "../conn_db.php";
 
-    
+    function tableComponent($connection, $table_name, $table_column = [], $table_row = []) {
 
+    // orders data
+    $get_table_data = "SELECT * FROM $table_name";
+    $query_table_data = mysqli_query($connection, $get_table_data);
+
+
+    if ($query_table_data && mysqli_num_rows($query_table_data) > 0) {
+        echo '
+            <table border="2">
+                <thead>
+                    <tr>';
+                        foreach ($table_column as  $col_name) {
+                            echo '<th>'.$col_name.'</th>';
+                        }
+        echo        '</tr>
+                </thead>
+                <tbody>
+        ';
+
+        while ($row_name = mysqli_fetch_assoc($query_table_data)) {
+            echo '
+                <tr class="values">';
+                    foreach($table_row as $row_values) {
+                        echo '<td>' . $row_name[$row_values] . '</td>';
+                    }
+            echo '</tr>';
+        }
+        echo '
+            </tbody>
+                </table>
+                <br /><br />
+        ';
+    }
+
+    }
 
 ?>
