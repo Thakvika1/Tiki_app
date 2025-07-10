ef7eb77ade4ab3fab915ad1694e99fd01131fb28
Thakvika1 <thakvika.thorn@gmail.com>
2025-07-10 15:59:40 +0700

    refactor orders dashboard

---
+++ dashboards/OrdersDashboard.php
@@ -0,0 +1,20 @@
+
+
+<?php
+    // connect to database 
+    include "../conn_db.php";
+
+    // table component 
+    include "../components/TableComponent.php";
+
+    // order table
+    echo tableComponent(
+        $conn, 
+        'orders',
+        'Orders', 
+        ['Order ID', 'Product ID', 'Customer Name', 'Customer Phone', 'Order Date'], 
+        ['order_id', 'product_id', 'customer_name', 'phone_number', 'created_at']
+    );
+
+
+?>
