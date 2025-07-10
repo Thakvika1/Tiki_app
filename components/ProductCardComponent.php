a8406c9771fbdbf9fa892701ffa48e5db22e58ff
Thakvika1 <thakvika.thorn@gmail.com>
2025-07-10 14:10:56 +0700

    refactor file

--- components/ProductCardComponent.php
+++ components/ProductCardComponent.php
@@ -7,7 +7,7 @@
                 <img id='image' src='$image' />
                 <h3>$title</h3>
                 <p>$$price</p>
-                <form method='post' action='./orders/confirm_order.php'>
+                <form method='post' action='./orders/ConfirmOrder.php'>
                     <input name='product_id' value='$product_id' type='hidden' />
                     <input name='title' value='$title' type='hidden' />
                     <input name='price' value='$price' type='hidden' />
